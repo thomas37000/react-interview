04926208cafe219b9cdb3e2a8cf36d6580799b0b
thomas <thomas.chalanson@sfr.fr>
2021-12-14 18:31:40 +0100

    do the pagination

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Select from "react-select";
+import ReactPaginate from "react-paginate";
 import { movies$ } from "../api/movies";
 import Card from "./Card/Card";
 import "./Movies.css";
 
-export default function Movies({options}) {
+export default function Movies({ options }) {
   const [moviesList, setMoviesList] = useState([]);
   const [likeMovie, setLikeMovie] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const moviePagination = 3;
+  const pagesVisited = pageNumber * moviePagination;
 
   const deleteMovie = (id) => {
     const deleteMoviesList = moviesList.filter((movie) => movie.id !== id);
@@ -34,22 +39,43 @@ export default function Movies({options}) {
 
   const fetchMovies =
     moviesList &&
-    moviesList.map((movie) => {
-      return (
-        <div className="container" key={movie.id}>
-          <Card
-            {...movie}
-            handleClick={() => deleteMovie(movie.id)}
-            onToggle={() => onToggleMovie()}
-            onChange={SelectGenres}
-          />
-        </div>
-      );
-    });
+    moviesList
+      .slice(pagesVisited, pagesVisited + moviePagination)
+      .map((movie) => {
+        return (
+          <div className="container" key={movie.id}>
+            <Card
+              {...movie}
+              handleClick={() => deleteMovie(movie.id)}
+              onToggle={() => onToggleMovie()}
+              onChange={SelectGenres}
+            />
+          </div>
+        );
+      });
+
+  const pageCount = Math.ceil(moviesList.length / moviePagination);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <>
-      <div className="movies-container">{fetchMovies}</div>
+      <div className="movies-container">
+        {fetchMovies}{" "}
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBtn"}
+          previousLinkClassName={"previousBtn"}
+          nextLinkClassName={"nextBtn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
+      </div>
     </>
   );
 }
